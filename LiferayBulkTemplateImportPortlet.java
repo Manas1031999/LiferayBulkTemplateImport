fe9aead69a7ccf50b621b0ec7272df64deafeca5
Manas Halder <125878051+Manas1031999@users.noreply.github.com>
2025-09-30 07:24:23 +0530

    First Commit

---
+++ LiferayBulkTemplateImportPortlet.java
@@ -0,0 +1,29 @@
+package com.liferay.bulk.template.importer.portlet;
+
+import com.liferay.bulk.template.importer.constants.LiferayBulkTemplateImportPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import javax.portlet.Portlet;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author user
+ */
+@Component(
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=LiferayBulkTemplateImport",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + LiferayBulkTemplateImportPortletKeys.LIFERAYBULKTEMPLATEIMPORT,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class LiferayBulkTemplateImportPortlet extends MVCPortlet {
+	
+	
+
+}
---
+++ LiferayBulkTemplateImportPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.bulk.template.importer.constants;
+
+/**
+ * @author user
+ */
+public class LiferayBulkTemplateImportPortletKeys {
+
+	public static final String LIFERAYBULKTEMPLATEIMPORT =
+		"com_liferay_bulk_template_importer_LiferayBulkTemplateImportPortlet";
+
+}
